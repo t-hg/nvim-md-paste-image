7464467f2a4d8773d4a5f92c7fac9f066d9bc6c7
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-07-21 11:56:40 +0200

    insert image in markdown format

--- lua/nvim-paste-image/init.lua
+++ lua/nvim-paste-image/init.lua
@@ -7,8 +7,11 @@ local function paste_image()
   math.randomseed(os.time())
   local random_number = math.random(1000, 9999)
   local date = os.date("%Y-%m-%d_%H-%M-%S")
-  local image_file = image_dir .. "/" .. date .. "_" .. random_number .. ".png"
-  os.execute("touch " .. image_file)
+  local image_file_name = date .. "_" .. random_number .. ".png"
+  local image_file_path = image_dir .. "/" .. image_file_name
+  os.execute("touch " .. image_file_path)
+  local row, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+  vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { "![image](" .. image_file_path .. ")" })
 end
 
 local function setup()
