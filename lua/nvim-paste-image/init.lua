6056eb2902b3e7667fc51aa445fec73fb426647f
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-07-21 12:06:59 +0200

    use deprecated unpack

--- lua/nvim-paste-image/init.lua
+++ lua/nvim-paste-image/init.lua
@@ -10,7 +10,7 @@ local function paste_image()
   local image_file_name = date .. "_" .. random_number .. ".png"
   local image_file_path = image_dir .. "/" .. image_file_name
   os.execute("touch " .. image_file_path)
-  local row, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
   vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { "![image](" .. image_file_path .. ")" })
 end
 
