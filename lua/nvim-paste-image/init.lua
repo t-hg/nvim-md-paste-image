bca28f11fee229054292ec7bc997af5b55b89162
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-07-21 10:54:17 +0200

    add setup function

--- lua/nvim-paste-image/init.lua
+++ lua/nvim-paste-image/init.lua
@@ -4,8 +4,14 @@ local function paste_image()
   print("function paste_image")
 end
 
-vim.api.nvim_create_user_command(
-  "PasteImage",
-  paste_image,
-  { nargs = 0, desc = "Paste image" }
-)
+local function setup()
+  vim.api.nvim_create_user_command(
+    "PasteImage",
+    paste_image,
+    { nargs = 0, desc = "Paste image" }
+  )
+end
+
+return {
+  setup = setup
+}
