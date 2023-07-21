0dc2e493b2a1edddca8a5b1893436b17de064f82
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-07-21 11:32:40 +0200

    add random number

--- lua/nvim-paste-image/init.lua
+++ lua/nvim-paste-image/init.lua
@@ -4,8 +4,10 @@ local function paste_image()
   print("function paste_image")
   local image_dir = "./_img"
   os.execute("mkdir -p " .. image_dir)
+  math.randomseed(os.time())
+  local random_number = math.random(1000, 9999)
   local date = os.date("%Y-%m-%d_%H-%M-%S")
-  local image_file = image_dir .. "/" .. date .. ".png"
+  local image_file = image_dir .. "/" .. date .. "_" .. random_number .. ".png"
   os.execute("touch " .. image_file)
 end
 
