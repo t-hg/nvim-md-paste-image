aa7967d2f57f00d35bda7d990b7343beca4abfad
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-07-21 10:45:02 +0200

    custom command

--- lua/nvim-paste-image/init.lua
+++ lua/nvim-paste-image/init.lua
@@ -1,7 +1,9 @@
-local setup = function()
-  print("hello from nvim-paste-image")
+local function paste_image()
+  print("function paste_image")
 end
 
-return {
-  setup = setup
-}
+vim.api.nvim_create_user_command(
+  "PasteImage",
+  paste_image,
+  { nargs = 0, desc = "Paste image" }
+)
