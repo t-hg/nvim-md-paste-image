a8515d0666722ab584d94b96ad44162d0cfa4e13
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-07-21 12:37:51 +0200

    Add error handling

--- lua/nvim-paste-image/init.lua
+++ lua/nvim-paste-image/init.lua
@@ -3,14 +3,17 @@ print("hello from nvim-paste-image")
 local function paste_image()
   print("function paste_image")
   local image_dir = "./_img"
-  os.execute("mkdir -p " .. image_dir)
+  if os.execute("mkdir -p " .. image_dir) ~= 0 then
+    error("Could not create image dir: " .. image_dir)
+  end
   math.randomseed(os.time())
   local random_number = math.random(1000, 9999)
   local date = os.date("%Y-%m-%d_%H-%M-%S")
   local image_file_name = date .. "_" .. random_number .. ".png"
   local image_file_path = image_dir .. "/" .. image_file_name
-  os.execute("touch " .. image_file_path)
-  os.execute("powershell.exe -c \"(get-clipboard -format image).save('.\\img.png')\"")
+  if os.execute("powershell.exe -c \"(get-clipboard -format image).save('" .. image_file_path .. "')\"") ~= 0 then
+    error("Failure in copying image data from clipboard to " .. image_file_path)
+  end
   local row, col = unpack(vim.api.nvim_win_get_cursor(0))
   vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, {" ![image](" .. image_file_path .. ")" })
 end
