1f8721d0fed70eb204eed965de76b55da876e088
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-07-21 11:21:59 +0200

    create image folder and file

--- lua/nvim-paste-image/init.lua
+++ lua/nvim-paste-image/init.lua
@@ -2,14 +2,15 @@ print("hello from nvim-paste-image")
 
 local function paste_image()
   print("function paste_image")
+  local image_dir = "./_img"
+  os.execute("mkdir -p " .. image_dir)
+  local date = os.date("%Y-%m-%d_%H-%M-%S")
+  local image_file = image_dir .. "/" .. date .. ".png"
+  os.execute("touch " .. image_file)
 end
 
 local function setup()
-  vim.api.nvim_create_user_command(
-    "PasteImage",
-    paste_image,
-    { nargs = 0, desc = "Paste image" }
-  )
+  vim.api.nvim_create_user_command("PasteImage", paste_image, { nargs = 0, desc = "Paste image" })
 end
 
 return {
